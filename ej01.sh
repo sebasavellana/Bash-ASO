4e06d295408abbe789e4668eec9274ee489a1916
sebasavellana <savellana.inf@gmail.com>
2020-10-17 00:11:58 +0200

    Bash Commit

--- ej01.sh
+++ ej01.sh
@@ -6,4 +6,3 @@ echo $(($1*$2))
 # Si se ejecuta desde la terminal con los argumentos 4 y 3 de la siguiente forma: 
 # bash multiply.sh 4 3 debería mostrar en la salida estándar 12.
 # Refactoriza el código de manera que devuelva dicha salida en una sola linea:
-
